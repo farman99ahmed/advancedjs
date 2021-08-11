f7c8999d551a633e9faa5c141e0dc8c8711033e8
Farman Ahmed <36335149+farman99ahmed@users.noreply.github.com>
2021-08-12 00:41:42 +0530

    Input validations

--- Assignment_2/Q2/index.js
+++ Assignment_2/Q2/index.js
@@ -4,6 +4,14 @@ class QueenAttack {
         this.position_2 = position_2;
     }
 
+    static validateInputs(position_1, position_2) {
+        if (position_1[0] > 7 || position_1[0] < 0 || position_1[1] > 7 || position_1[1] < 0)
+            return false
+        if (position_2[0] > 7 || position_2[0] < 0 || position_2[1] > 7 || position_2[1] < 0)
+            return false
+        return true
+    }
+
     canAttack() {
         if (this.position_1[0] == this.position_2[0])
             return true;
@@ -18,8 +26,12 @@ class QueenAttack {
     }
 }
 
-const position_1 = [0, 0];
-const position_2 = [7, 7];
+const position_1 = [7, 8];
+const position_2 = [7, 6];
 
-const board = new QueenAttack(position_1, position_2);
-console.log(board.canAttack());
+if (QueenAttack.validateInputs(position_1, position_2)) {
+    const board = new QueenAttack(position_1, position_2);
+    console.log(board.canAttack());
+} else {
+    console.log("Invalid Inputs");
+}
